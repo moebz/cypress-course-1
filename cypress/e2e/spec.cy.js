c7b3e37d2ee4dfe16d44876ba8e8c914cecdc390
Moises B <mbaez091@gmail.com>
2024-01-27 22:28:38 -0300

    cypress clicking and checking content example

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,16 @@
+describe("template spec", () => {
+  it("passes", () => {
+    cy.visit("/fundamentals");
+    cy.contains(/Your tests will exist in a describe block/i).should(
+      "not.be.visible"
+    );
+    cy.get('[data-test="accordion-item-1"] div[role="button"]').click();
+    cy.contains(/Your tests will exist in a describe block/i).should(
+      "be.visible"
+    );
+    cy.get('[data-test="accordion-item-1"] div[role="button"]').click();
+    cy.contains(/Your tests will exist in a describe block/i).should(
+      "not.be.visible"
+    );
+  });
+});
